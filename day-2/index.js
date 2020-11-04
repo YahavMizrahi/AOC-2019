c5fa753b335197ece7a076df5cf94896217ea863
YahavMizrahi <yahav283@gmail.com>
2020-11-04 15:51:59 +0200

    day-2 task-1, Done

---
+++ day-2/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+let data = fs.readFileSync( `${__dirname}/data.txt`,'utf-8' );
+data = data.split(",").map(num => +num); // convert to number
+
+
+const opcode = {
+  "1" : ( i, input ) => input[input[i+2]]=input[input[i]]+input[input[i+1]],
+  "2" : ( i, input ) => input[input[i+2]]=input[input[i]]*input[input[i+1]]
+}
+
+const firstTask = (input) => {
+
+  input[1] = 12;
+  input[2] = 2;
+  let operation =0; 
+  let currentOperation = input[operation];
+
+  while(currentOperation !== 99){
+    if( opcode[ currentOperation ]) {
+      opcode[ currentOperation ]( operation + 1, input );
+    }
+
+    operation += 4;
+    currentOperation = input[operation];
+  }
+
+  return input[0];
+
+};
+
+
+const secondTask = (input) => {
+  return "day-2 second task";
+};
+
+console.log("First problem:", firstTask(data));
+console.log("Second problem:", secondTask(data));
+
