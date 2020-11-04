4db4964eb137f95c73e809213bd9b96c5d5b9043
YahavMizrahi <yahav283@gmail.com>
2020-11-04 16:54:12 +0200

    day-2, Done

--- day-2/index.js
+++ day-2/index.js
@@ -8,10 +8,10 @@ const opcode = {
   2 : ( i, input ) => input[ input[ i + 2 ]] = input[ input[i]] * input[ input[ i + 1 ]]
 }
 
-const firstTask = (input) => {
+const firstTask = ([...input],noun=12,verb=2) => {
 
-  input[1] = 12;
-  input[2] = 2;
+  input[1] = noun;
+  input[2] = verb;
   let operation =0; 
   let currentOperation = input[operation];
 
@@ -29,8 +29,12 @@ const firstTask = (input) => {
 };
 
 
-const secondTask = (input) => {
-  return "day-2 second task";
+const secondTask = ( input ) => {
+  for( let i = 0; i < 100; i++){
+    for( let j = 0; j < 100; j++){
+      if( firstTask( input, i, j ) === 19690720) return 100 * i + j;
+    }
+  }
 };
 
 console.log("First problem:", firstTask(data));
